f3e7c9d48f13f6e18c2d62c934497fb6dd236c63
s.kudinov <s.kudinov@fun-box.ru>
2018-06-21 12:25:59 +0400

    Add yaml output format

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const Crafter = require('./Crafter.js');
+const yaml = require('yamljs');
+const program = require('commander');
 
-if (process.argv.length !== 3) {
-  console.log('Usage: crafter file.apib');
-  process.exit(1);
-}
+program
+  .usage('[options] <apib file>')
+  .option('-f, --format [format]', 'output format of the Parse Result: yaml|json', /^(yaml|json)$/, 'yaml')
+  .parse(process.argv);
 
-const data = fs.readFileSync(process.argv[2], {encoding: 'utf-8'});
+if (program.args.length === 0)
+  program.help();
+
+const data = fs.readFileSync(program.args[0], {encoding: 'utf-8'});
 const result = Crafter.parse(data);
-console.log(JSON.stringify(result.toRefract(), null, 2));
+if (program.format === 'json') {
+  console.log(JSON.stringify(result.toRefract(), null, 2));
+} else {
+  console.log(yaml.stringify(result.toRefract(), Infinity, 2));
+}
