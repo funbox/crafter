943844c6e790f052a14ff78d923b11864212e3c9
Ge11ert <gellert94@mail.ru>
2018-07-10 13:54:41 +0300

    Exclude tests with an expected error in fixtures regeneration task

--- scripts/regenerate-fixtures.js
+++ scripts/regenerate-fixtures.js
@@ -2,12 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const parseApibFile = require('../parseApibFile');
 
+const excludedDirs = ['fixtures-with-errors'];
+
 processApibFiles(path.resolve('tests/fixtures'));
 
 function processApibFiles(dir) {
   const dirContent = fs.readdirSync(dir);
   dirContent.forEach((item) => {
-    if (fs.statSync(path.join(dir, item)).isDirectory()) {
+    if (fs.statSync(path.join(dir, item)).isDirectory() && !excludedDirs.includes(item)) {
       processApibFiles(path.join(dir, item));
     } else if (path.extname(item) === '.apib') {
       const fileName = path.join(dir, item);
