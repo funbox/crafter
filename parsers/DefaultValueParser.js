73eb6b734d9ce6924b254be308b99793fc92ea22
Ge11ert <gellert94@mail.ru>
2018-08-22 14:53:32 +0300

    Process nested values in Default section

--- parsers/DefaultValueParser.js
+++ parsers/DefaultValueParser.js
@@ -1,21 +1,24 @@
 const SectionTypes = require('../SectionTypes');
 const utils = require('../utils');
+const SignatureParser = require('../SignatureParser');
 const DefaultValueElement = require('./elements/DefaultValueElement');
 
 const defaultValueRegex = /^[Dd]efault:\s*`?(.+?)`?$/;
+const listTypedDefaultValueRegex = /^[Dd]efault$/;
 
 module.exports = (Parsers) => {
   Parsers.DefaultValueParser = Object.assign(Object.create(require('./AbstractParser')), {
     processSignature(node, context) {
       const text = utils.nodeText(node.firstChild, context.sourceLines);
-      const val = defaultValueRegex.exec(text)[1];
-      return [utils.nextNode(node), new DefaultValueElement(val)];
+      const val = defaultValueRegex.exec(text);
+      const defaultValueElement = new DefaultValueElement((val && val[1]) || undefined);
+      return [(node.firstChild.next && node.firstChild.next.firstChild) || utils.nextNode(node), defaultValueElement];
     },
 
     sectionType(node, context) {
       if (node.type === 'item') {
         const text = utils.nodeText(node.firstChild, context.sourceLines);
-        if (defaultValueRegex.exec(text)) {
+        if (defaultValueRegex.exec(text) || listTypedDefaultValueRegex.exec(text)) {
           return SectionTypes.defaultValue;
         }
       }
@@ -23,8 +26,24 @@ module.exports = (Parsers) => {
       return SectionTypes.undefined;
     },
 
+    nestedSectionType(node, context) {
+      if (node.type === 'item' && this.sectionType(node.parent.parent, context) !== SectionTypes.undefined) {
+        return SectionTypes.msonAttribute;
+      }
+
+      return SectionTypes.undefined;
+    },
+
     processDescription(node, context, result) {
       return [node, result];
+    },
+
+    processNestedSection(node, context, result) {
+      const text = utils.nodeText(node.firstChild, context.sourceLines);
+      const value = new SignatureParser(text);
+      result.value = value.name;
+
+      return [utils.nextNode(node), result];
     },
   });
 };
