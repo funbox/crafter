f506ad5f58ae43f317e115ea5767826d0c64e36e
Ge11ert <gellert94@mail.ru>
2019-02-05 23:59:31 +0300

    Split Crafter.parse into synchronous methods and callback methods

--- Crafter.js
+++ Crafter.js
@@ -6,6 +6,51 @@ const Logger = require('./utils').Logger;
 
 const Parsers = {};
 
+function maybeCallback(cb) {
+  return typeof cb === 'function' ? cb : () => { throw new Error('Call of an asynchronous function without a callback'); };
+}
+
+function getOptions(options, defaultOptions) {
+  return options === null || options === undefined || typeof options === 'function'
+    ? defaultOptions
+    : options;
+}
+
+function parse(source, contextOptions, callback) {
+  callback = maybeCallback(callback || contextOptions);
+  const options = getOptions(contextOptions, {});
+  try {
+    const result = parseSync(source, options);
+    callback(undefined, result);
+  } catch (error) {
+    callback(error);
+  }
+}
+
+function parseFile(file, contextOptions, callback) {
+  callback = maybeCallback(callback || contextOptions);
+  const options = getOptions(contextOptions, {});
+  try {
+    const result = parseFileSync(file, options);
+    callback(undefined, result);
+  } catch (error) {
+    callback(error);
+  }
+}
+
+function parseSync(source, contextOptions) {
+  const ast = utils.markdownSourceToAST(source);
+  contextOptions.logger = contextOptions.logger || new Logger();
+  const context = new Context(source, Parsers, contextOptions);
+  return Parsers.BlueprintParser.parse(ast.firstChild, context)[1];
+}
+
+function parseFileSync(file, contextOptions = {}) {
+  contextOptions.entryDir = path.dirname(file);
+  contextOptions.logger = contextOptions.logger || new Logger();
+  return parseSync(fs.readFileSync(file, { encoding: 'utf-8' }), contextOptions);
+}
+
 fs.readdirSync(path.join(__dirname, 'parsers')).forEach((pFile) => {
   if (/Parser.js$/.exec(pFile)) {
     const defineParser = require(`./parsers/${pFile}`); // eslint-disable-line import/no-dynamic-require
@@ -16,16 +61,8 @@ fs.readdirSync(path.join(__dirname, 'parsers')).forEach((pFile) => {
 });
 
 module.exports = {
-  parse(source, contextOptions) {
-    const ast = utils.markdownSourceToAST(source);
-    contextOptions.logger = contextOptions.logger || new Logger();
-    const context = new Context(source, Parsers, contextOptions);
-    return Parsers.BlueprintParser.parse(ast.firstChild, context)[1];
-  },
-
-  parseFile(file, contextOptions = {}) {
-    contextOptions.entryDir = path.dirname(file);
-    contextOptions.logger = contextOptions.logger || new Logger();
-    return this.parse(fs.readFileSync(file, { encoding: 'utf-8' }), contextOptions);
-  },
+  parse,
+  parseSync,
+  parseFile,
+  parseFileSync,
 };
