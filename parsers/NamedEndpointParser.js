dee4d52027d6715def7ed2b46f3c4873ec9c5e7a
Ge11ert <gellert94@mail.ru>
2019-02-26 16:33:36 +0300

    Add NamedEndpointParser

---
+++ parsers/NamedEndpointParser.js
@@ -0,0 +1,27 @@
+const SectionTypes = require('../SectionTypes');
+const RegExpStrings = require('../RegExpStrings');
+const utils = require('../utils');
+
+const NamedEndpointHeaderRegex = new RegExp(`^${RegExpStrings.symbolIdentifier}\\s+\\[${RegExpStrings.requestMethods}\\s+${RegExpStrings.uriTemplate}](\\s+${RegExpStrings.resourcePrototype})?$`);
+
+module.exports = (Parsers) => {
+  Parsers.NamedEndpointParser = Object.assign(
+    Object.create(require('./AbstractParser')),
+    Parsers.ResourceParser,
+    {
+      allowLeavingNode: false,
+
+      sectionType(node, context) {
+        if (node.type === 'heading') {
+          const subject = utils.headerText(node, context.sourceLines);
+
+          if (NamedEndpointHeaderRegex.exec(subject)) {
+            return SectionTypes.namedAction;
+          }
+        }
+
+        return SectionTypes.undefined;
+      },
+    },
+  );
+};
