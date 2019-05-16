6d0fb41d05f83706802569e50c4a69c8e3466ffa
Ge11ert <gellert94@mail.ru>
2019-05-16 13:24:44 +0300

    Add MessageParser/Element

---
+++ parsers/MessageParser.js
@@ -0,0 +1,88 @@
+const SectionTypes = require('../SectionTypes');
+const RegExpStrings = require('../RegExpStrings');
+const utils = require('../utils');
+const MessageElement = require('./elements/MessageElement');
+const StringElement = require('./elements/StringElement');
+const BodyElement = require('./elements/BodyElement');
+
+const MessageHeaderRegex = new RegExp(`^[Mm]essage\\s*(${RegExpStrings.symbolIdentifier})?$`);
+
+module.exports = (Parsers) => {
+  Parsers.MessageParser = Object.assign(Object.create(require('./AbstractParser')), {
+    processSignature(node, context) {
+      let title = '';
+      const matchData = MessageHeaderRegex.exec(utils.headerText(node, context.sourceLines));
+      const sourceMap = utils.makeGenericSourceMap(node, context.sourceLines, context.sourceBuffer, context.linefeedOffsets);
+
+      if (matchData[1]) {
+        title = matchData[1];
+      }
+
+      const titleEl = title ? new StringElement(title, sourceMap) : undefined;
+      const result = new MessageElement(titleEl, sourceMap);
+
+      return [utils.nextNode(node), result];
+    },
+
+    sectionType(node, context) {
+      if (node.type === 'heading') {
+        const subject = utils.headerText(node, context.sourceLines);
+
+        if (MessageHeaderRegex.exec(subject)) {
+          return SectionTypes.message;
+        }
+      }
+
+      return SectionTypes.undefined;
+    },
+
+    nestedSectionType(node, context) {
+      return SectionTypes.calculateSectionType(node, context, [
+        Parsers.BodyParser,
+        Parsers.AttributesParser,
+      ]);
+    },
+
+    upperSectionType(node, context) {
+      return SectionTypes.calculateSectionType(node, context, [
+        Parsers.MessageParser,
+        Parsers.ResourceParser,
+        Parsers.SubgroupParser,
+        Parsers.ResourceGroupParser,
+        Parsers.DataStructureGroupParser,
+        Parsers.ResourcePrototypeParser,
+        Parsers.ResourcePrototypesParser,
+      ]);
+    },
+
+    processNestedSection(node, context, result) {
+      let nextNode;
+      let childResult;
+
+      if (Parsers.BodyParser.sectionType(node, context) !== SectionTypes.undefined) {
+        [nextNode, childResult] = Parsers.BodyParser.parse(node, context);
+        result.content.push(childResult);
+      } else {
+        [nextNode, childResult] = Parsers.AttributesParser.parse(node, context);
+        result.content.push(childResult);
+      }
+
+      return [nextNode, result];
+    },
+
+    finalize(context, result) {
+      const hasCustomBody = result.content.find(item => (item instanceof BodyElement));
+
+      if (!hasCustomBody) {
+        const body = result.getBody(context.typeResolver.types);
+        if (Object.keys(body).length > 0) {
+          const bodyElement = new BodyElement(body);
+          result.content.push(bodyElement);
+        }
+      }
+
+      return result;
+    },
+  });
+  return true;
+};
---
+++ parsers/elements/MessageElement.js
@@ -0,0 +1,50 @@
+const Refract = require('../../Refract');
+const utils = require('../../utils');
+const SourceMapElement = require('./SourceMapElement');
+
+class MessageElement {
+  constructor(title, sourceMap) {
+    this.title = title;
+    this.sourceMap = sourceMap;
+    this.description = null;
+    this.content = [];
+  }
+
+  toRefract(sourceMapsEnabled) {
+    const sourceMapEl = sourceMapsEnabled && this.sourceMap ? new SourceMapElement(this.sourceMap.byteBlocks, this.sourceMap.file) : null;
+
+    const result = {
+      element: Refract.elements.message,
+      content: this.content.map(c => c.toRefract(sourceMapsEnabled)),
+    };
+
+    if (this.title) {
+      result.meta = {
+        title: {
+          element: Refract.elements.string,
+          content: this.title.toRefract(sourceMapsEnabled),
+        },
+      };
+    }
+
+    if (this.description) {
+      result.content.unshift(this.description.toRefract(sourceMapsEnabled));
+    }
+
+    if (sourceMapEl) {
+      result.attributes = result.attributes || {};
+      result.attributes.sourceMap = sourceMapEl.toRefract();
+    }
+    return result;
+  }
+
+  getBody(resolvedTypes) {
+    let body = {};
+    this.content.forEach(item => {
+      body = utils.mergeBodies(body, item.getBody(resolvedTypes));
+    });
+    return body.value !== undefined ? body.value : body;
+  }
+}
+
+module.exports = MessageElement;
