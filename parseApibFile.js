618a2f4db59901cc73315d2c7ce45f0bd53eb39a
komuroe <komuroe@gmail.com>
2019-08-06 20:24:55 +0300

    Fix a value returned from parseApibFile

--- parseApibFile.js
+++ parseApibFile.js
@@ -2,7 +2,7 @@ const yaml = require('yamljs');
 const Crafter = require('./Crafter.js');
 
 module.exports = function parseApibFile(fileName, outputFormat, sourceMapsEnabled) {
-  const result = Crafter.parseFileSync(fileName, { sourceMapsEnabled });
+  const result = Crafter.parseFileSync(fileName, { sourceMapsEnabled })[0];
   if (outputFormat === 'json') {
     return JSON.stringify(result.toRefract(sourceMapsEnabled), null, 2);
   }
