531111dc2e5b195a7856772ed88b6763ed61b4e2
Ge11ert <gellert94@mail.ru>
2019-09-30 15:37:14 +0300

    Add a script to build docker image after package publish

---
+++ build-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IMAGE_NAME="funbox/crafter"
+CRAFTER_VERSION=`npm view @funbox/crafter dist-tags.latest`
+
+docker build -t $IMAGE_NAME:$CRAFTER_VERSION - < Dockerfile
+docker push $IMAGE_NAME:$CRAFTER_VERSION
+docker tag $IMAGE_NAME:$CRAFTER_VERSION $IMAGE_NAME:latest
