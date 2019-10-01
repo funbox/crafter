d80f2891ce5e1bdf52f55ab488f37f17847f37b6
Ge11ert <gellert94@mail.ru>
2019-10-01 17:13:01 +0300

    Add a separate command to push the latest version of the image

--- build-docker.sh
+++ build-docker.sh
@@ -6,3 +6,4 @@ CRAFTER_VERSION=`npm view @funbox/crafter dist-tags.latest`
 docker build -t $IMAGE_NAME:$CRAFTER_VERSION - < Dockerfile
 docker push $IMAGE_NAME:$CRAFTER_VERSION
 docker tag $IMAGE_NAME:$CRAFTER_VERSION $IMAGE_NAME:latest
+docker push $IMAGE_NAME:latest
