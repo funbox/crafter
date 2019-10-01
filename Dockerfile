d6b15e08c081313eee1deb25d83761acbc9b87da
goganchic <goganchic@gmail.com>
2019-10-01 21:51:10 +0300

    Specify Crafter version in a container

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM node:10.24.1
 
 LABEL org.label-schema.description="Image for launching npm package \"@funbox/crafter\""
 
+ARG CRAFTER_VERSION=latest
 WORKDIR /app
 
-RUN npm install -g --silent @funbox/crafter@latest
+RUN npm install -g --silent @funbox/crafter@${CRAFTER_VERSION}
 
 ENTRYPOINT ["crafter"]
--- build-docker.sh
+++ build-docker.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME="funbox/crafter"
 CRAFTER_VERSION=`npm view @funbox/crafter dist-tags.latest`
 
-docker build -t $IMAGE_NAME:$CRAFTER_VERSION - < Dockerfile
+docker build --build-arg CRAFTER_VERSION=$CRAFTER_VERSION -t $IMAGE_NAME:$CRAFTER_VERSION - < Dockerfile
 docker push $IMAGE_NAME:$CRAFTER_VERSION
 docker tag $IMAGE_NAME:$CRAFTER_VERSION $IMAGE_NAME:latest
 docker push $IMAGE_NAME:latest
