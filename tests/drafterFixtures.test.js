479292d37c22e5062dc55017de87b4c18f99a7ae
newagebegins <newagebegins@gmail.com>
2019-10-28 20:09:58 +1000

    Specify relevant drafter commit

--- tests/drafterFixtures.test.js
+++ tests/drafterFixtures.test.js
@@ -5,7 +5,7 @@ const Crafter = require('../Crafter');
 
 const DRAFTER_REPO_URL = 'https://github.com/apiaryio/drafter.git';
 const DRAFTER_BRANCH = 'master';
-const DRAFTER_COMMIT = '88e79114c40850bf28c4909ac50615e4d7122ad5'; // Jul 30, 2019
+const DRAFTER_COMMIT = 'bda1cadd50bc2448b19aa9d0da5bc4cca938aefd'; // Oct 17, 2019
 const FIXTURES_PATH = 'drafter/test/fixtures';
 const APIB_REGEX = /\.apib$/;
 
