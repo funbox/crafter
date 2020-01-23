f53ad39e5c4603755ba81e932e69ea9497eff745
goganchic <goganchic@gmail.com>
2020-01-23 17:47:42 +0300

    Add a description of ArrayElement

--- parsers/elements/ArrayElement.js
+++ parsers/elements/ArrayElement.js
@@ -1,18 +1,58 @@
 const utils = require('../../utils');
 
+/**
+ * Массив
+ *
+ * исходный текст:
+ * + Attributes
+ *   + foo (array[string])
+ *
+ * дерево:
+ * AttributesElement
+ *   content: ValueMemberElement
+ *     content: ObjectElement
+ *       propertyMembsers:
+ *         - PropertyMemberElement
+ *           value: ValueMemberElement
+ *             content: ArrayElement <--
+ *               members:
+ *                 - ValueMemberElement
+ */
 class ArrayElement {
+  /**
+   *
+   * @param {ValueMemberElement[]} members - вложенные типы данных, например для array[string] это string
+   */
   constructor(members) {
     this.members = members;
   }
 
+  /**
+   * @param {boolean} sourceMapsEnabled
+   * @param {boolean} isFixed
+   */
   toRefract(sourceMapsEnabled, isFixed) {
     return this.members.map(element => element.toRefract(sourceMapsEnabled, isFixed));
   }
 
+  /**
+   *
+   * @param {Set} resolvedTypes - типы из TypeResolver
+   * @param {string[]} namedTypesChain
+   */
   getBody(resolvedTypes, namedTypesChain = []) {
     return this.members.map(member => member.getBody(resolvedTypes, namedTypesChain));
   }
 
+  /**
+   *
+   * @param {Set} resolvedTypes - типы из TypeResolver
+   * @param {object} flags - флаги генерации JSON Schema
+   * @param {boolean} flags.isFixed
+   * @param {boolean} flags.isFixedType
+   * @param {boolean} flags.isNullable
+   * @param {boolean} flags.skipTypesInlining
+   */
   getSchema(resolvedTypes, flags = {}) {
     const schema = { type: 'array' };
     const localFlags = { ...flags };
