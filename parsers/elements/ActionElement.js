491da592441de02a4495d6292afdba704ee5d296
goganchic <goganchic@gmail.com>
2020-01-24 17:39:37 +0300

    Add a description of ActionElement

--- parsers/elements/ActionElement.js
+++ parsers/elements/ActionElement.js
@@ -2,18 +2,66 @@ const Refract = require('../../Refract');
 const RequestElement = require('./RequestElement');
 const SourceMapElement = require('./SourceMapElement');
 
+/**
+ * Action - связка URL + метод + 1 и более Request + 1 или более Response
+ * если запросов в тексте документации нет - то один запрос будет создан автоматически в методе ActionParser.finalize
+ *
+ * Action элементы группируются в элемент Resource в рамках одного URL, т.е. для примера ниже будет ResourceElement в котором два ActionElement
+ *
+ * # Users [/users]
+ *
+ * ## [GET]
+ * + Response 200
+ * ## [POST]
+ * + Response 200
+ *
+ * А для такого примера будет ResourceElement в котором только один ActionElement
+ *
+ * # List users [GET /users/]
+ * + Response 200
+ *
+ * Дерево:
+ *
+ * ResourceElement
+ *   actions:
+ *     - ActionElement <--
+ *       requests:
+ *         - RequestElement
+ *       responses:
+ *         - ResponseElement
+ */
 class ActionElement {
+  /**
+   * @param {string} title
+   * @param {string} href
+   * @param {StringElement} method
+   */
   constructor(title, href, method) {
     this.title = title;
     this.href = href;
     this.method = method;
+    /**
+     * @type {RequestElement[]}
+     */
     this.requests = [];
+    /**
+     * @type {ResponseElement[]}
+     */
     this.responses = [];
+    /**
+     * @type {ParametersElement}
+     */
     this.parameters = null;
+    /**
+     * @type {DescriptionElement}
+     */
     this.description = null;
     this.sourceMap = null;
   }
 
+  /**
+   * @param {boolean} sourceMapsEnabled
+   */
   toRefract(sourceMapsEnabled) {
     const sourceMapEl = sourceMapsEnabled && this.sourceMap ? new SourceMapElement(this.sourceMap.byteBlocks, this.sourceMap.file) : null;
 
