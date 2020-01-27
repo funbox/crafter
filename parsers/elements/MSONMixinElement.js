99c7e6897a306c118378b9d88f39eeab21eb5475
goganchic <goganchic@gmail.com>
2020-01-27 12:36:34 +0300

    Add a description of MSONMixinElement

--- parsers/elements/MSONMixinElement.js
+++ parsers/elements/MSONMixinElement.js
@@ -1,11 +1,27 @@
 const Refract = require('../../Refract');
 
+/**
+ * Include секция
+ *
+ * Пример:
+ *
+ * + foo
+ *   + Include MyType <--
+ *   + bar (string, required)
+ */
 class MSONMixinElement {
+  /**
+   * @param {string} className
+   * @param sourceMap
+   */
   constructor(className, sourceMap) {
     this.className = className;
     this.sourceMap = sourceMap;
   }
 
+  /**
+   * @param {boolean} sourceMapsEnabled
+   */
   toRefract() {
     return {
       element: Refract.elements.ref,
@@ -19,11 +35,22 @@ class MSONMixinElement {
     };
   }
 
+  /**
+   * @param {Set} resolvedTypes - типы из TypeResolver
+   */
   getBody(resolvedTypes) {
     const typeEl = resolvedTypes[this.className];
     return typeEl.getBody(resolvedTypes);
   }
 
+  /**
+   * @param {Set} resolvedTypes - типы из TypeResolver
+   * @param {object} flags - флаги генерации JSON Schema
+   * @param {boolean} flags.isFixed
+   * @param {boolean} flags.isFixedType
+   * @param {boolean} flags.isNullable
+   * @param {boolean} flags.skipTypesInlining
+   */
   getSchema(resolvedTypes, flags = {}) {
     const typeEl = resolvedTypes[this.className];
     return typeEl.getSchema(resolvedTypes, flags);
