98488411f7e276661709a5a52e9e2b5926a636bd
goganchic <goganchic@gmail.com>
2020-06-09 10:58:20 +0300

    Add the option to run crafter in language server mode

--- cli.js
+++ cli.js
@@ -10,12 +10,13 @@ program
   .option('-f, --format [format]', 'output format of the Parse Result: yaml|json', /^(yaml|json)$/, 'yaml')
   .option('-s, --sourcemap', 'export sourcemap in the Parse Result')
   .option('-d, --debug', 'debug mode')
+  .option('-l, --langserver', 'language server parsing mode')
   .parse(process.argv);
 
 if (program.args.length === 0) {
   program.help();
 }
 
-parseApibFile(program.args[0], program.format, program.sourcemap, program.debug, false, { logger: new Logger() }).then(result => {
+parseApibFile(program.args[0], program.format, program.sourcemap, program.debug, program.langserver, { logger: new Logger() }).then(result => {
   console.log(result);
 });
