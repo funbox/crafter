3e25529e9db886dc19215c4f5f77b7491bb6d777
Ge11ert <gellert94@mail.ru>
2021-02-25 15:33:27 +0300

    Use getSourcePosZeroBased helper function to get node text

--- utils/node/nodeText.js
+++ utils/node/nodeText.js
@@ -1,25 +1,31 @@
+const getSourcePosZeroBased = require('../getSourcePosZeroBased');
+
 module.exports = function nodeText(node, sourceLines) {
   if (!node) {
     return '';
   }
 
   const localSourceLines = node.sourceLines || sourceLines;
-  const [startline, startcolumn] = node.sourcepos[0];
-  const [endline, endcolumn] = node.sourcepos[1];
+  const {
+    startLineIndex,
+    endLineIndex,
+    startColumnIndex,
+    endColumnIndex,
+  } = getSourcePosZeroBased(node);
   const keepWhitespaces = node.type === 'code_block' || node.type === 'item';
 
   const result = [];
 
-  if (startline === endline) {
-    result.push(localSourceLines[startline - 1].slice(startcolumn - 1, endcolumn));
+  if (startLineIndex === endLineIndex) {
+    result.push(localSourceLines[startLineIndex].slice(startColumnIndex, endColumnIndex + 1));
   } else {
-    result.push(localSourceLines[startline - 1].slice(startcolumn - 1));
+    result.push(localSourceLines[startLineIndex].slice(startColumnIndex));
 
-    for (let i = startline + 1; i < endline; i += 1) {
-      result.push(localSourceLines[i - 1]);
+    for (let i = startLineIndex + 1; i < endLineIndex; i += 1) {
+      result.push(localSourceLines[i]);
     }
 
-    result.push(localSourceLines[endline - 1].slice(0, endcolumn));
+    result.push(localSourceLines[endLineIndex].slice(0, endColumnIndex + 1));
   }
 
   return result.map(line => (keepWhitespaces ? line : line.trim())).join('\n').trim();
