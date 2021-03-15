649520db1ea091dba075acdd915285e051b733b2
mdnsk <m.donsk@ya.ru>
2021-03-15 22:14:38 +0300

    Fix regexp of Resource Prototype in language server mode

--- RegExpStrings.js
+++ RegExpStrings.js
@@ -23,5 +23,5 @@ module.exports = {
   symbolIdentifier: '([^\\]\\[\\(\\)]+)',
   resourcePrototype: '\\(([^\\]\\[\\(\\)]+)\\)',
   mediaType: '(\\s*\\(([^\\)]*)\\))',
-  languageServerResourcePrototype: '\\((((([^\\]\\[\\(\\)]+)\\)?))?)?',
+  languageServerResourcePrototype: '\\((?:(?:([^\\]\\[\\(\\)]+)\\)?))?',
 };
