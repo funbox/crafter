7fb94b8d1d2d94f7a8dc8892db2048a489598b0c
Ge11ert <gellert94@mail.ru>
2023-02-22 02:05:46 +0300

    Delete redundant script

--- build-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IMAGE_NAME="funbox/crafter"
-CRAFTER_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-
-docker build --build-arg CRAFTER_VERSION=$CRAFTER_VERSION -t $IMAGE_NAME:$CRAFTER_VERSION - < Dockerfile
-docker push $IMAGE_NAME:$CRAFTER_VERSION
-docker tag $IMAGE_NAME:$CRAFTER_VERSION $IMAGE_NAME:latest
-docker push $IMAGE_NAME:latest
